d4efa583f6b9f4135eaa554526f235f4f617cfd4
NestaDV <ntirampebanestor15@gmail.com>
2023-05-08 12:51:37 +0200

    cours python lundi

---
+++ Student.py
@@ -0,0 +1,2 @@
+def showStudent():
+ print("bonjour Nestor")
--- main.py
+++ main.py
@@ -1,1 +1,101 @@
 print('Hello')
+from Student import showStudent
+
+if __name__ == '__main__':
+    showStudent()
+    
+    
+
+#Aller au contenu
+#Utiliser Gmail avec un lecteur d'écran
+#Conversations
+#0,08 Go utilisés sur 15 Go
+#Conditions d'utilisation · Confidentialité · Règlement du programme
+#Dernière activité sur le compte : il y a 3 jours
+#Détails
+#x = "Python est magique"
+#print(x)
+#def myfunction(nom):
+#   print("Bonjour " +nom)
+#if __name__ == '__main__':
+    def myfunction(nom):
+        print("Bonjour " +nom)
+    myfunction("Guy")
+
+   #x,y,z=1,2,3
+   #print(x)
+   #print(y)
+   #print(z)"""
+   #print("Hello class")
+   #a = 4
+   #b = 6
+   #c = "Bonjour"
+   #d = " Tout le monde" 
+   # Les operateurs d'affectation
+   #print(10+5)
+   #print(10-5)
+   #print(10*5)
+   #print(10/5)
+   #print(10%3)
+   #print(5**2)
+   # Les operateurs de comparaison
+   # inferieur ou égal : <=
+   # superieur ou égal : >=
+   # inferieur : <
+   # superieur : >
+   # égal : ==
+   # different : !=
+   #print(a+b)
+   #print(c+d)
+   #x = "Bonjour"
+   #x = "L\'arbre"
+   #print(x)
+   #print(len(x))
+   #print(x[3])
+   #for a in x:
+       #print(a)
+"""salutation = "Bonjour"
+for s in salutation :
+    print(s)
+
+a,b = 5,10  
+
+if a == b or a > 7:
+    print(a)
+elif a < b :
+    print(a)
+else :
+    print(b)"""
+"""i = 1
+while i < 10:
+    i+=1
+    if i==5 :
+        continue
+    print(i)
+    #break
+    #i += 1"""
+#main.py
+#Affichage de main.py en cours...
+"""   
+class Personne:
+        def __init__(self,first_name,last_name,age):
+           self.first_name = first_name
+           self.last_name = last_name
+           self.age = age
+            
+        def __str__(self):
+                return f"{self.first_name}({self.age})"
+        def showPersonne(self):
+                print(self.first_name,self.age)
+s = Personne('ntirampeba',"Nestor",23)
+        
+    
+print(s.first_name,s.age)
+    
+"""
+        
+    #Student={
+      #nom ='Ntirampeba',
+      #prenom='Nestor',   
+    
+#}
